9b2864737f2a510a646569524cf27d4288dd8134
fajrulaulia <auliafajrul7@outlook.co.id>
2019-10-02 17:11:43 +0700

    soal n 5

---
+++ 05.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+function ReadJSON(){
+    fs.readFile('./data.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        console.log('File data:', jsonString) 
+    })
+}
+
+
+
+
+ReadJSON()
